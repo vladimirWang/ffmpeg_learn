94c40f02264833dd77b14562ecc110d39432b131
fernando <413114463@qq.com>
2025-12-31 16:48:00 +0800

    resolve conflict

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include "calc.h"
+#include <libavutil/log.h>
+#include <libavformat/avformat.h>
+
+void testLog()
+{
+    av_log_set_level(AV_LOG_DEBUG);
+    // av_log(NULL, AV_LOG_INFO, "hello ffmpeg %d", 120);
+    printf("%d\n", 12);
+}
+
 int main()
 {
-    int result = sum(1, 4);
-    printf("%d\n", result);
+    testLog();
+    // int ans = testPriv();
+    // if (ans != 0)
+    // {
+    //     return ans
+    // }
     return 0;
 }
